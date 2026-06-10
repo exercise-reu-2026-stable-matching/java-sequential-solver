e99b83ecbf40c76cc80da46152f8626a4e3daca9
Will Bradley <68962413+a2435191@users.noreply.github.com>
2026-06-10 15:16:52 -0400

    Find all the places PII with n=3 gets into a cycle

--- Main.java
+++ Main.java
@@ -1,23 +1,44 @@
-import java.util.Optional;
-import java.util.Random;
+import java.util.stream.Stream;
+import java.util.function.BiFunction;
+import java.util.function.Supplier;
+import java.util.Arrays;
+import java.util.List;
 
 class Main {
-    /** Find a (randomly-generated) `Prefs` of size `n` that causes
-     * `runOrCycle` to cycle. */
-    static Prefs findCycle(Random rng, int n) {
-        while (true) {
-            Prefs prefs = Prefs.random(rng, n);
-            PII pii = new PII(prefs);
-            Permutation initial = Permutation.identity(n);
-            Optional<Permutation> result = pii.runOrCycle(initial);
-            if (!result.isPresent())
-                return prefs;
-        }
+
+    private static <T, U, R> Stream<R> productMap(Stream<T> ts, Supplier<Stream<U>> us, BiFunction<T, U, R> f) {
+        return ts.flatMap(t -> us.get().map(u -> f.apply(t, u)));
+    }
+
+    // TODO this has duplicates up to permutation
+    private static Stream<Permutation[]> allPrefsGo(int n, int fuel) {
+        if (fuel == 0) return Stream.ofNullable(new Permutation[0]);
+        Supplier<Stream<Permutation>> allPerms = () -> Permutation.all(n);
+        
+        return productMap(allPrefsGo(n, fuel - 1), allPerms, (tl, hd) -> {
+            Permutation[] next = Arrays.copyOf(tl, tl.length + 1);
+            next[next.length - 1] = hd;
+            return next;
+        });
+    }
+
+    private static Stream<Permutation[]> allPrefs(int n) {
+        return allPrefsGo(n, n);
     }
 
     public static void main(String[] args) {
-        Random rng = new Random(1);
-        Prefs cyclePrefs = findCycle(rng, 3);
-        System.out.println(cyclePrefs);
+        final int n = 3;
+
+        Stream<Permutation[]> allMalePrefs = allPrefs(n);
+        Supplier<Stream<Permutation[]>> allFemalePrefs = () -> allPrefs(n);
+        Stream<Prefs> allPrefs = productMap(allMalePrefs, allFemalePrefs, Prefs::new);
+        Permutation initialMatching = Permutation.identity(n);
+        
+        List<Prefs> s = allPrefs
+            .filter(p -> new PII(p).runOrCycle(initialMatching).isEmpty())
+            .toList();
+        System.out.println(s.size());
+        for (Prefs p : s)
+            System.out.println(p + "\n");
     }
 }
