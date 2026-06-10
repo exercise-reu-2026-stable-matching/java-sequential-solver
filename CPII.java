8ec2fa8f5f3664e168e57dea87799af6abd9a36d
Will Bradley <68962413+a2435191@users.noreply.github.com>
2026-06-09 22:03:02 -0400

    stub out CPII

---
+++ CPII.java
@@ -0,0 +1,10 @@
+class CPII extends PIIBase {
+    CPII(Prefs prefs) {
+        super(prefs);
+    }
+
+    @Override
+    Permutation iterationPhase(Permutation mensMatches) {
+        throw new RuntimeException("not implemented"); // TODO
+    }
+}
