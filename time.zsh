5fae33c2866b4163d7b1c3a0bcb0e0c2720bf311
Will Bradley <68962413+a2435191@users.noreply.github.com>
2026-06-11 13:49:23 -0400

    Add async-profiler options to the `time.zsh` script

--- time.zsh
+++ time.zsh
@@ -1,3 +1,5 @@
 javac Main.java
 
-time java Main 256 128
+time java -XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints\
+    -agentpath:/opt/homebrew/lib/libasyncProfiler.dylib=start,event=cpu,file=flamegraph.html\
+    Main 256 128
