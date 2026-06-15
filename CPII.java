edfdab72e4cfb53bfdd5227172fe4a15b8e73993
Will Bradley <68962413+a2435191@users.noreply.github.com>
2026-06-15 09:59:41 -0400

    Implement CPII core functionality

--- CPII.java
+++ CPII.java
@@ -1,6 +1,97 @@
+import java.util.Arrays;
+import java.util.Random;
+
 class CPII extends PIIBase {
+    private static final int UNMATCHED = -1;
+
+    private static int[] allUnmatched(int n) {
+        int[] out = new int[n];
+        Arrays.fill(out, UNMATCHED);
+        return out;
+    }
+
     @Override
-    Permutation iterationPhase(Prefs prefs, Permutation mensMatches) {
-        throw new RuntimeException("not implemented"); // TODO
+    Permutation initiationPhase(Random rng, int n) {
+        return new Permutation(allUnmatched(n), allUnmatched(n));
+    }
+
+    private static boolean isUnstablePair(Prefs prefs, Permutation mensMatches, int y, int x) {
+        assert y != UNMATCHED;
+        assert x != UNMATCHED;
+
+        int matchedWoman = mensMatches.get(y);
+        int matchedMan = mensMatches.getInverse(x);
+
+        return (matchedWoman == UNMATCHED || prefs.malePrefs(y, x)   < prefs.malePrefs(y, matchedWoman)) 
+            && (matchedMan == UNMATCHED   || prefs.femalePrefs(x, y) < prefs.femalePrefs(x, matchedMan));
+    }
+
+    /** Return the male-dominant unstable pair for each row, or -1 if no such pair exists */
+    private static int[] maleDominantUnstablePairs(Prefs prefs, Permutation mensMatches) {
+        final int n = prefs.n();
+        int[] out = allUnmatched(n);
+
+        for (int y = 0; y < n; y++) {
+            for (int x = 0; x < n; x++) {
+                if (isUnstablePair(prefs, mensMatches, y, x)) {
+                    int oldX = out[y];
+                    if (oldX == UNMATCHED || prefs.malePrefs(y, x) < prefs.malePrefs(y, oldX))
+                        out[y] = x;
+                }
+            }
+        }
+
+        return out;
+    }
+
+    /** Return the male-female-dominant unstable pair for each column, or -1 if no such pair exists.
+     */
+    private static int[] maleFemaleDominantUnstablePairs(Prefs prefs, Permutation mensMatches, int[] maleDominantUnstablePairs) {
+        final int n = prefs.n();
+        int[] out = allUnmatched(n); // by column
+
+        for (int y = 0; y < n; y++) {
+            int x = maleDominantUnstablePairs[y];
+            if (x != UNMATCHED) {
+                int oldY = out[x];
+                if (oldY == UNMATCHED || prefs.femalePrefs(x, y) < prefs.femalePrefs(x, oldY))
+                    out[x] = y;
+            }
+        }
+
+        return out;
+    }
+    
+    // ks is like mensMatches
+    @Override
+    Permutation iterationPhase(Prefs prefs, Permutation ks) {
+        int[] bs = maleDominantUnstablePairs(prefs, ks);
+        int[] cs = maleFemaleDominantUnstablePairs(prefs, ks, bs);
+        
+        final int n = prefs.n();
+        // start totally empty
+        int[] nextMatches = allUnmatched(n);
+        int[] nextMatchesInv = allUnmatched(n);
+
+        // add C_i
+        for (int x = 0; x < n; x++) {
+            int y = cs[x];
+            if (y != UNMATCHED) {
+                nextMatches[y] = x;
+                nextMatchesInv[x] = y;
+            }
+        }
+
+        // add each element of K_i as long as it doesn't share a member with a pair in C_i
+        for (int y = 0; y < n; y++) {
+            if (nextMatches[y] == UNMATCHED) {
+                int x = ks.get(y);
+                nextMatches[y] = x;
+                assert nextMatchesInv[x] == UNMATCHED;
+                nextMatchesInv[x] = y;
+            }
+        }
+
+        return new Permutation(nextMatches, nextMatchesInv);
     }
 }
