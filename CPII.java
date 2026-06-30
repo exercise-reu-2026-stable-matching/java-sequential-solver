2eb4639acd8c1e6fd1f8f6ec261e5be75363d401
Will Bradley <68962413+a2435191@users.noreply.github.com>
2026-06-30 09:17:40 -0400

    Skip proposals we know won't work

--- CPII.java
+++ CPII.java
@@ -28,13 +28,18 @@ class CPII extends PIIBase {
     }
 
     private final Prefs prefs;
-    private final int n; // size
-    private final int[] maleCurrentPartnerRanks;
+    /** Size */
+    private final int n;
+    /** In `maleDominantUnstablePairs` we can skip looking at highly-ranked (low rank number) women 
+     * we already considered and broke up with. This should take the total runtime from O(n^2) per iteration
+     * to O(n^2) total
+      */
+    private final int[] maleCurrentProposalRanks;
 
     CPII(Prefs prefs) {
         this.prefs = prefs;
         n = prefs.n();
-        maleCurrentPartnerRanks = allUnmatched(n);
+        maleCurrentProposalRanks = allUnmatched(n);
     }
 
     /** Return the male-dominant unstable pair for each row, or -1 if no such pair exists */
@@ -43,10 +48,13 @@ class CPII extends PIIBase {
 
         for (int m = 0; m < n; m++) {
             Permutation malePrefs = prefs.malePrefs()[m];
-            for (int rank = 0; rank < n; rank++) {
+            int currentRank = maleCurrentProposalRanks[m];
+            int startRank = currentRank == UNMATCHED ? 0 : currentRank;
+            for (int rank = startRank; rank < n; rank++) {
                 int w = malePrefs.getInverse(rank);
                 if (isUnstablePair(prefs, mensMatches, m, w)) {
                     out[m] = w;
+                    maleCurrentProposalRanks[m] = rank;
                     break;
                 }
             }
