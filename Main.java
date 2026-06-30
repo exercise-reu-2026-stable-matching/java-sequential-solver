8ff9689bd1a29fc8b36ed62ed72578e4ee241f43
Will Bradley <68962413+a2435191@users.noreply.github.com>
2026-06-30 09:37:08 -0400

    Driver program

--- Main.java
+++ Main.java
@@ -1,93 +1,25 @@
-import java.util.List;
-import java.util.Objects;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
 import java.util.Random;
-import java.util.function.BiFunction;
-import java.util.function.Supplier;
-import java.util.stream.Stream;
 
 class Main {
-    /** Overwrites with the next sequence in the lexicographic ordering of `[0, k)^n`.
-     * Returns true iff we're not at the end of the sequence.
-     */
-    private static boolean nextSeq(long k, int[] arr) {
-        final int n = arr.length;
-        for (int i = n - 1; i >= 0; i--) {
-            if (arr[i] < k - 1) {
-                arr[i]++;
-                return true;
-            } else {
-                arr[i] = 0;
-                if (i == 0)
-                    return false;
+    /** Run CPII on a bunch of random inputs and spit out how long each takes to converge */
+    public static void main(String[] args) throws IOException {
+        final Random rng = new Random(1);
+        final int nSize  = Integer.parseInt(args[0]);
+        final int nSamples = Integer.parseInt(args[1]);
+        final Permutation initial = Permutation.allUnmatched(nSize);
+
+        // Buffer to avoid overhead of System.out.println each time
+        try (BufferedOutputStream out = new BufferedOutputStream(System.out)) {
+            for (int i = 0; i < nSamples; i++) {
+                Prefs prefs = Prefs.random(rng, nSize);
+                FastCPII cpii = new FastCPII(prefs);
+                var result = cpii.runOrCycle(prefs, initial);
+                assert result.fst().isPresent();
+                int itersToConverge = result.snd();
+                out.write((itersToConverge + "\n").getBytes());
             }
         }
-        throw new AssertionError("unreachable!");
-    }
-
-    /** The Cartesian product `[0, k)` `n` times total */
-    private static Stream<int[]> products(long k, int n) {
-        return Stream.iterate(new int[n], arr -> {
-            if (nextSeq(k, arr)) return arr; else return null;
-        }).takeWhile(Objects::nonNull);
-    }
-
-    private static long factorial(int n) {
-        long out = 1;
-        for (int i = 2; i <= n; i++)
-            out *= i;
-        return out;
-    }
-
-    /** All `(n!)^n` many `n x n` preference matrices */
-    private static Stream<Permutation[]> allPrefs(int n) {
-        final List<Permutation> allPerms = Permutation.all(n).toList();
-        return products(factorial(n), n).map(indices -> {
-            Permutation[] out = new Permutation[n];
-            for (int i = 0; i < n; i++)
-                out[i] = allPerms.get(indices[i]);
-            return out;
-        });
-    }
-
-    private static <T, U, R> Stream<R> productMap(Stream<T> ts, Supplier<Stream<U>> us, BiFunction<T, U, R> f) {
-        return ts.flatMap(t -> us.get().map(u -> f.apply(t, u)));
-    }
-
-    static Pair<Boolean, Integer> countCycles(PIIBase pii, Prefs prefs, Permutation init) {
-        var result = pii.runOrCycle(prefs, init);
-        return new Pair<>(result.fst().isPresent(), result.snd());
-    }
-
-    public static void main(String[] args) {
-        Random rng = new Random(1);
-        final int nIters = Integer.parseInt(args[0]);
-        final int nSize  = Integer.parseInt(args[1]);
-        PII pii = new PII();
-        CPII cpii = new CPII();
-
-        System.out.println("pii_converged,pii_diverged,pii_converged_total_iters,pii_diverged_total_iters,cpii_total_iters");
-        Permutation identity = Permutation.identity(nSize);
-        Permutation empty = Permutation.allUnmatched(nSize);
-
-        long nPIIConverging = 0, nItersForPIIOfConverging = 0, nItersForPIIOfDiverging = 0,
-             nItersForCPII = 0;
-
-        for (int i = 0; i < nIters; i++) {
-            Prefs prefs = Prefs.random(rng, nSize);
-            var piiResult = countCycles(pii, prefs, identity);
-            var cpiiResult = countCycles(cpii, prefs, empty);
-
-            if (piiResult.fst()) {
-                nPIIConverging++;
-                nItersForPIIOfConverging += piiResult.snd();
-            } else
-                nItersForPIIOfDiverging += piiResult.snd();
-
-            assert cpiiResult.fst() : prefs;
-            nItersForCPII += cpiiResult.snd();
-        }
-
-        System.out.printf("%d,%d,%d,%d,%d\n", 
-            nPIIConverging, nIters - nPIIConverging, nItersForPIIOfConverging, nItersForPIIOfDiverging, nItersForCPII);
     }
 }
