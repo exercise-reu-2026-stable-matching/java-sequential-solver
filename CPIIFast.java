66cd1ef6da449b26f993be54d41826c32bf6cbf4
Will Bradley <68962413+a2435191@users.noreply.github.com>
2026-06-30 10:34:36 -0400

    Also exclude women who we can't possibly prefer to our current match

--- CPIIFast.java
+++ CPIIFast.java
@@ -1,7 +1,7 @@
 
 class CPIIFast extends CPII {
     /** In `maleDominantUnstablePairs` we can skip looking at highly-ranked (low rank number) women 
-     * we already considered and broke up with. This should take the total runtime from O(n^2) per iteration
+     * we already considered and broke up with. This and ignoring women who won't ever match with us should take the total runtime from O(n^2) per iteration
      * to O(n^2) total
       */
     private final int[] maleCurrentProposalRanks;
@@ -20,7 +20,9 @@ class CPIIFast extends CPII {
             Permutation malePrefs = prefs.malePrefs()[m];
             int currentRank = maleCurrentProposalRanks[m];
             int startRank = currentRank == UNMATCHED ? 0 : currentRank;
-            for (int rank = startRank; rank < n; rank++) {
+            int matchedWoman = mensMatches.get(m);
+            int matchedRank = matchedWoman == UNMATCHED ? n : malePrefs.get(matchedWoman);
+            for (int rank = startRank; rank < matchedRank; rank++) {
                 int w = malePrefs.getInverse(rank);
                 if (isUnstablePair(prefs, mensMatches, m, w)) {
                     out[m] = w;
