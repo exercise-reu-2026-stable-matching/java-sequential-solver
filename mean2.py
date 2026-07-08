fea4a454d26f18273aece840ae216078c0269c11
Will Bradley <68962413+a2435191@users.noreply.github.com>
2026-07-08 09:50:43 -0400

    Program to aggregate results

---
+++ mean2.py
@@ -0,0 +1,20 @@
+#!/opt/homebrew/bin/python3.10
+
+import sys
+from collections import defaultdict
+
+# for each i
+sizes = defaultdict(lambda: (0, 0))
+
+n = None
+for line in sys.stdin:
+    n2, i, size = [int(x) for x in line.split(",")]
+    assert n == None or n == n2
+    n = n2
+    
+    total, count = sizes[i]
+    sizes[i] = (total + size, count + 1)
+   
+print("i,|B_i|/n") 
+for k in sorted(sizes.keys()):
+    print(f"{k},{sizes[k][0] / sizes[k][1] / n:.5f}", flush=True)
