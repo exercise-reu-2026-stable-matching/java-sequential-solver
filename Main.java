125db3ee4f5220d9266037cfa6933bee83320102
Will Bradley <68962413+a2435191@users.noreply.github.com>
2026-08-13 13:52:16 -0400

    Update harness

--- Main.java
+++ Main.java
@@ -4,20 +4,21 @@ import java.util.Random;
 class Main {
     /** Run CPII on a bunch of random inputs and spit out how long each takes to converge */
     public static void main(String[] args) throws IOException {
-        final Random rng = new Random(1);
+        final int seed = args.length >= 3 ? Integer.parseInt(args[2]) : 1;
+        final Random rng = new Random(seed);
         final int nSize  = Integer.parseInt(args[0]);
         final int nSamples = Integer.parseInt(args[1]);
         final Permutation initial = Permutation.allUnmatched(nSize);
 
         for (int i = 0; i < nSamples; i++) {
-            System.err.println("Sample " + i);
+            if (i % 100 == 0)
+                System.err.println("Sample " + i);
             final Prefs prefs = Prefs.random(rng, nSize);
 
             CPII fast = new CPIIFast(nSize);
-            // int itersToConvergeFast = 
-            fast.countIters(prefs, initial);
-            // out.write((itersToConvergeFast + "\n").getBytes());
+            int itersToConvergeFast = fast.countIters(prefs, initial);
+            System.out.println(itersToConvergeFast);
         }
-        CPII.out.flush();
+        // CPII.out.flush();
     }
 }
